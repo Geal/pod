fd1046265b422ed1849625d4ededddbf6d775160
Geoffroy Couprie <geo.couprie@gmail.com>
2013-06-08 12:47:59 +0200

    be less verbose about class loading

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -5,7 +5,7 @@ spl_autoload_register(function ($class) {
     if (0 === strpos($class, 'PHPZ\\')) {
         $class = str_replace('\\', '/', $class);
         $file = sprintf("%s/%s.php", PHPZ_DIR, $class);
-        printf("Auto loading class '%s' (%s).\n", $class, $file);
+        //printf("Auto loading class '%s' (%s).\n", $class, $file);
 
         require $file;
     }
