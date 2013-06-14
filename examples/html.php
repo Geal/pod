badd4d380301af5c3771ac532ff0a7cbd866921a
Geoffroy Couprie <geo.couprie@gmail.com>
2013-06-14 17:27:05 +0200

    add real file examples

---
+++ examples/html.php
@@ -0,0 +1,11 @@
+<html>
+<head></head>
+<body>
+<?php
+  $a = "Hello " + "World!"
+?>
+  <h1><?php
+$print $a 
+?></h1>
+</body>
+</html>
---
+++ examples/simple.php
@@ -0,0 +1,7 @@
+<?php
+ $a = 1+1
+ print a
+ abc = (x, y) -> {
+   ret x+ y
+ }
+?>
