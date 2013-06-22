7269c8573904c5f25719aadf4916d468459d7a21
Geoffroy Couprie <geo.couprie@gmail.com>
2013-06-22 17:07:12 +0200

    improve the basic example

--- examples/html.php
+++ examples/html.php
@@ -2,11 +2,28 @@
 <head></head>
 <body>
 <?php
-  $a = "Hello " . "World!";
+$a = "Hello " . "World!";
 
 ?>
   <h1><?php
-print $a ;
+print htmlentities($a );
 ?></h1>
+
+<?php
+class A{
+    public $value;
+static public function mul($x, $y){ return $x * $y ;
+}
+public function __construct($val){ $this->value = $val ;
+}
+public function show(){ print htmlentities($this->value );
+}
+};
+
+$str = "<script>alert('pouet')</script>";
+$obj = new A($str);
+$obj->show();
+
+?>
 </body>
 </html>
