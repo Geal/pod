Geal/pod

commit 715df46529b6a86b0aadd612a7a8e20fb638259d

functions can begin with underscores

src/POD/Preprocessor.php
@@ -127,7 +127,7 @@ function parameter_list() {
 }
 
 function func_name() {
-  return Seq(alpha(), lists(C(alphanum(), is("_"))));
+  return Seq(lists(C(alphanum(), is("_"))));
 }
 
 function funcall() {

commit a1dfd43731c1df9618cf58e7fd8e10244599634e

Fix regression caused by 715df46529b6a86b0aadd612a7a8e20fb638259d
A function name cannot start with a number.
This fixes simple.pod example.
Btw, rename func_name to funname for consistency

Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

examples/simple.php
@@ -1,7 +1,8 @@
 <?php
 $a = 1+1;
- abc = (x, y) -> {
-   ret x+ y
- }
-print "hello"
+function abc($x, $y){
+   return $x. $y;
+};
+print htmlentities("hello");
+
 ?>

src/POD/Preprocessor.php
@@ -124,12 +124,12 @@ function parameter_list() {
          );
 }
 
-function func_name() {
-  return Seq(lists(C(alphanum(), is("_"))));
+function funname() {
+  return Seq(C(alpha(), is("_")), lists(C(alphanum(), is("_"))));
 }
 
 function funcall() {
-  return Seq(Seq(opt(is('$')), func_name()), rs(), is('('), rs(), parameter_list());
+  return Seq(Seq(opt(is('$')), funname()), rs(), is('('), rs(), parameter_list());
 }
 
 function raw_expression() {
@@ -175,7 +175,7 @@ function fun() {
 }
 
 function fundec() {
-  return Seq(replace(rs(), "function "), func_name(), replace(Seq(rs(), is("="), rs()), ""), fun());
+  return Seq(replace(rs(), "function "), funname(), replace(Seq(rs(), is("="), rs()), ""), fun());
 }
 
 function class_statement() {
@@ -207,7 +207,7 @@ function classbody() {
   );
 }
 function classname() {
-  return func_name();
+  return funname();
 }
 
 function classdec() {

tests/unit/Preprocessor.Test.php
@@ -123,6 +123,7 @@ class ParserTest extends lime_test {
     $this->verif($f, "abc = (x) -> {\n ret 1 }", "", "function abc(\$x){\n return 1 ;\n}", "parse function declarations");
     $this->verif($f, "__construct  =  ( val ) -> { this.value = val }", "", "function __construct( \$val ){ \$this->value = \$val ;\n}", "parse function declarations");
     $this->verif($f, "abc = (x, y) -> { ret x + y }" , "", "function abc(\$x, \$y){ return \$x . \$y ;\n}", "parse function declarations");
+    $this->verif($f, "9abc = (x, y) -> { ret x + y }" , "", NULL, "parse function declarations");
   }
 
   public function operatorStatementTest() {